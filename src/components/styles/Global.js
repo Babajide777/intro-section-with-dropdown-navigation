93ef74eae787aa9402320c8832b957b19ecb8b2e
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-05-17 06:25:40 -0500

    default app settings done

---
+++ src/components/styles/Global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@500;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+    font-size: ${({ theme }) => theme.fontSize.p};
+    font-weight: ${({ theme }) => theme.fontWeight.small};
+    font-family: ${({ theme }) => theme.fontFamily.epilogue};
+    background-color: ${({ theme }) => theme.color.almostWhite};
+    #root{
+      max-width: 100%;
+      width: 100%
+    }
+}
+
+img{
+    width: 100%
+}
+
+`;
+
+export default GlobalStyles;
---
+++ src/components/styles/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  color: {
+    almostWhite: "hsl(0, 0%, 98%)",
+    mediumGray: "hsl(0, 0%, 41%)",
+    almostBlack: "hsl(0, 0%, 8%)",
+    white: "#FFF",
+    black: "#000",
+  },
+  fontFamily: {
+    epilogue: "'Epilogue', sans-serif",
+  },
+  fontSize: {
+    p: "18px",
+  },
+  fontWeight: {
+    small: 500,
+    bold: 700,
+  },
+  mobile: "600px",
+};
+
+export default theme;
